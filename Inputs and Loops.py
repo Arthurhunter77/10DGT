2afa5afb4b06b887d254f4b378022e3c2a4e9e7e
Arthurhunter77 <arthur.brook@kingsway.school.nz>
2025-05-07 10:54:43 +1200

    Add files via upload

---
+++ Inputs and Loops.py
@@ -0,0 +1,19 @@
+'''Inputs and loops
+Arthur Brook
+07/05/25
+v1.0'''
+
+#ask the user a question and store their answer in a variable
+#ask the use for their name and store it
+username = input("Kia ora! What is your name? ")
+print(username)
+
+#ask the user for two numbers and then add them together
+num1 = int(input("What is your first number? "))
+num2 = int(input("What is your second number? "))
+print(f"Your first number is {num1}")
+print(f"Your second number is {num2}")
+
+print(f"Added together, {num1} + {num2} = {num1+num2}.")
+
+#to comment a line of code out, do ctrl + /
