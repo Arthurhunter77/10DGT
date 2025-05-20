d7f0b3b84d5a9c65e30df7dd4b2781518c53c48c
Arthurhunter77 <arthur.brook@kingsway.school.nz>
2025-05-21 10:09:12 +1200

    Add files via upload
    Starting tutorial videos

---
+++ AP_Basics.py
@@ -0,0 +1,13 @@
+'''Area/Perimeter Basics
+21/05/25
+Arthur Brook
+v.1.0
+fav_num = 7
+num_string = "5"
+print(fav_num*2)
+print(num_string*5)
+
+love_programming = True
+greeting = 'hello world'
+print(f"{greeting}! my favorite number is {fav_num}, and it is {love_programming} that I love programming.")
+'''
